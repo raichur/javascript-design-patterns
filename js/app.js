b7247e4a7f53fc1b0c491f26656eed2c2fa6ab6d
Joshua Raichur <hello@joshuaraichur.com>
2015-02-12 16:09:53 +0530

    remove unnecessary code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 // Click Cat
-var count = 0, cats = ['cat1', 'cat2', 'cat3', 'cat4'];
 $('img').click(function(){
-  if(cats.indexOf($(this).parent().attr('class')) + 1){
-    $(this).parent().find('#count').text(parseInt($(this).parent().find('#count').text()) + 1);
-  }
+  $(this).parent().find('#count').text(parseInt($(this).parent().find('#count').text()) + 1);
 });
