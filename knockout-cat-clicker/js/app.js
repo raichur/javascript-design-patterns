cc4a3b385449646cae32fbeac2454724bf14c5ab
Joshua Raichur <hello@joshuaraichur.com>
2015-02-14 11:34:03 +0530

    data binding

--- knockout-cat-clicker/js/app.js
+++ knockout-cat-clicker/js/app.js
@@ -1,5 +1,11 @@
-var ViewModel = function() {
+var VkiewModel = function() {
+  this.clickCount = ko.observable(0);
+  this.name = ko.observable('Tabby');
+  this.imgSrc = ko.observable('img/cat1.jpg');
 
+  this.incrementCounter = function() {
+    this.clickCount(this.clickCount() + 1);
+  };
 };
 
-ko.applyBindings(new ViewModel());
+ko.applyBindings(new VkiewModel());
