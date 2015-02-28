bbd7d398e5b6b8979189f076042ebeeed4ef9af8
Joshua Raichur <hello@joshuaraichur.com>
2015-02-28 12:54:15 +0530

    priority

--- todo/js/models/todo.js
+++ todo/js/models/todo.js
@@ -13,7 +13,15 @@ var app = app || {};
 		// and ensure that each todo created has `title` and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			priority: false
+		},
+
+		// Toggle priority class
+		togglePriority: function() {
+			this.save({
+				priority: !this.get('priority')
+			});
 		},
 
 		// Toggle the `completed` state of this todo item.
